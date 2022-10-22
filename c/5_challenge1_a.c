2df27dab9b86a04ff0ae48173cf0d1ae5ef7360a
Sara <yojan.bustamante@udea.edu.co>
2022-10-22 17:59:22 -0500

    formatted

--- c/5_challenge1_a.c
+++ c/5_challenge1_a.c
@@ -12,9 +12,8 @@ main ()
   perimeter = (width + height) * 2.0;
   area = width * height;
 
-  printf ("The width of the rectangle is %f, and its height is %f\n",
-	  width,
-	  height);
+  printf ("The width of the rectangle is %f, and its height is %f\n", width,
+          height);
 
   printf ("The perimeter is: %f\n", perimeter);
   printf ("The area is: %f\n", area);
